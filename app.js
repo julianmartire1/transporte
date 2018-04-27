151f5da7e5c6850467654dcc715bbbfdfd65832e
julianmartire1 <julianmartire.jm@gmail.com>
2018-04-27 13:32:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 
 //CORSE
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://transportesq.000webhostapp.com");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept, Origin, Authorization, X-API-KEY, Access-Control-Allow-Request-Method");
     res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE");
     next();
